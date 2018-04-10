b0777e3a5bc4b7df7a6b3366da3b909969eef613
zihaowang <zihaowang@Ctrip.com>
2018-04-10 19:24:52 +0800

    create my-cat-vue

---
+++ src/components/Navi/Navi.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        {{message}}
+    </div>
+</template>
+
+<script>
+    export default {
+        el:'#app',
+        data(){
+            return{
+                message:'Hello Vue!!!!'
+            }
+        }
+    }
+</script>
+
+<style>
+#app{
+    font-size: 18px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Navi from './components/Navi/Navi.vue'
+
+Vue.use(ElementUI)
+
+new Vue({
+  el: '#app',
+  render: h => h(Navi)
+})
